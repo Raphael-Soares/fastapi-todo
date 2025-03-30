73c1f6ddeda761323355a2e45dd12c5af3717287
Raphael-Soares <soaresphaell@gmail.com>
2025-03-30 17:10:22 -0300

    TEST: create user test

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_wity_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_wity_id)
+    return user_wity_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,26 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'password': 'passwd',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'test',
+        'email': 'test@test.com',
+        'id': 1,
+    }
