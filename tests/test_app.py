26b89ef923dad750cbe7c660d709512b7cca7558
Raphael-Soares <soaresphaell@gmail.com>
2025-03-30 18:27:49 -0300

    FEAT & TEST: created a few CRUD endpoints

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,3 +24,72 @@ def test_create_user(client):
         'email': 'test@test.com',
         'id': 1,
     }
+
+
+def test_read_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'test',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'test',
+                'email': 'test@test.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': 'passwd',
+            'username': 'updated user',
+            'email': 'updated@test.com',
+            'id': 1,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'updated user',
+        'email': 'updated@test.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_not_found_delete_user(client):
+    response = client.delete('/users/9999999')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_not_found_update_user(client):
+    response = client.put(
+        '/users/999999999999999',
+        json={
+            'password': 'passwd',
+            'username': 'updated user',
+            'email': 'updated@test.com',
+            'id': 9999999,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
